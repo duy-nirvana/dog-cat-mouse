21942726937ef9c9b3addb196248265318aa1aa9
duynirvana <trankhanhduy8599@gmail.com>
2019-09-11 13:39:42 +0700

    Create new cat1 & mouse1, let dog eat cat, cat eat mouse

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var Dog = require('./Dog')
+var Dog = require('./Dog');
+var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
 var dog1 = new Dog('Gấu Chó');
+var cat1 = new Cat('Doremon');
+var mouse1 = new Mouse('Mickey');
+
+
 dog1.sayHi();
+
+cat1.eat(mouse1);
+dog1.eat(cat1);
+
+console.log(dog1);
+console.log(cat1);
